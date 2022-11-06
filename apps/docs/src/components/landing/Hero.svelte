3f1c6ed3815fb04a879262952030624234849972
Emilio Barradas <decalerss@gmail.com>
2022-11-06 01:37:24 -0400

    Sync

--- apps/docs/src/components/landing/Hero.svelte
+++ apps/docs/src/components/landing/Hero.svelte
@@ -12,7 +12,7 @@
 		draggable="false"
 	/>
 
-	<h1 class="font-semibold text-7xl leading-normal text-[#a40e13]">Cereza</h1>
+	<h1 class="font-semibold text-7xl leading-normal">Cereza</h1>
 	<p class="text-2xl font-medium max-w-[32rem]">
 		An open-source Docker container deployment and management system.
 	</p>
